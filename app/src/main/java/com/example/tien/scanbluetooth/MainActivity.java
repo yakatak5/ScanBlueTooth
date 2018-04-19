a05411559ddbf149bf21f957738ce67d7b8d7578
yakatak5 <yuskhan@gmail.com>
2018-04-19 15:43:43 -0500

    fixed url

--- app/src/main/java/com/example/tien/scanbluetooth/MainActivity.java
+++ app/src/main/java/com/example/tien/scanbluetooth/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.tien.scanbluetooth;
 
-import android.bluetooth.BluetoothDevice;
-import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Bundle;
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.util.Log;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.TimingLogger;
 import android.view.Menu;
 import android.view.View;
@@ -20,21 +15,38 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import java.lang.reflect.Method;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
    public ListView listView;
     public ArrayList<BluetoothDevice> mDeviceList = new ArrayList<BluetoothDevice>();
-
+    RequestQueue requestQueue;
+    String insertUrl = "http://10.176.138.38/log/insert.php";
     private long startnow;
     private long endnow;
     private static final String MYTAG = "MyActivity";
     TimingLogger timings = new TimingLogger(MYTAG, "MainActivity");
+    String deviceName;
+    String deviceHardwareAddress;
+    int rssi;
+    Date currentTime;
+    long timeEnd;
+    TextView rssi_msg = (TextView) findViewById(R.id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+
+
     }
 
     @Override
@@ -63,28 +80,54 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             timings.addSplit("work B");
               startnow = System.nanoTime();
-            Date currentTime = Calendar.getInstance().getTime();
+            currentTime = Calendar.getInstance().getTime();
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 mDeviceList = new ArrayList<BluetoothDevice>();
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+                rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
                // String name = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress();
-                TextView rssi_msg = (TextView) findViewById(R.id.textView);
+                deviceName = device.getName();
+                deviceHardwareAddress = device.getAddress();
+
                 endnow = System.nanoTime();
-                long timeEnd = (endnow - startnow)/1000000;
+                timeEnd = (endnow - startnow)/1000000;
                // rssi_msg.setText( "\n" + deviceHardwareAddress +  " | " + rssi_msg.getText()  + " | " +  deviceName + " |  "  + rssi + " dBm"   +  " | time process: " + timeEnd+ " milliseconds" );
     rssi_msg.setText("\n"  + rssi_msg.getText() +   currentTime + " Device Name " + deviceName  + " | Device Address  " + deviceHardwareAddress + "\n" +  " Signal Strength : " + rssi + "dBm.\n"  + " | time process: " + timeEnd + " milliseconds" + "\n\n");
                 //rssi_msg.setText("Device Name " + deviceName + " | " + deviceHardwareAddress + " | " + rssi + " dBm\n" +  " | time process: " + timeEx + " milliseconds");
 
+                StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        System.out.println(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> parameters  = new HashMap<String, String>();
+                        parameters.put("date",currentTime.toString());
+                        parameters.put("name",deviceName);
+                        parameters.put("address",deviceHardwareAddress);
+                        parameters.put("strength", Integer.toString(rssi));
+                        parameters.put("start", (timeEnd + "ms") );
+
+                        return parameters;
+                    }
+                };
+                requestQueue.add(request);
             }
 
-            timings.addSplit("work C");
-            timings.dumpToLog();
         }
-    };
+
+        };
+
 
 
 
