0ef44ed7db7790b2eeeb93348c7cf081ad5561f4
yakatak5 <yuskhan@gmail.com>
2018-04-20 17:26:24 -0500

    Rearranging

--- app/src/main/java/com/example/tien/scanbluetooth/MainActivity.java
+++ app/src/main/java/com/example/tien/scanbluetooth/MainActivity.java
@@ -37,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<BluetoothDevice> mDeviceList = new ArrayList<BluetoothDevice>();
     RequestQueue requestQueue;
     String insertUrl = "http://10.176.138.38/log/insert.php";
-    private long startnow;
-    private long endnow;
+    private long startnow = 0;
+    private long endnow = 0;
     private static final String MYTAG = "MyActivity";
     TimingLogger timings = new TimingLogger(MYTAG, "MainActivity");
     String deviceName;
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     int rssi;
     Date currentTime;
     long timeEnd;
-    TextView rssi_msg = (TextView) findViewById(R.id.textView);
+    TextView rssi_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+
+
 
         Button boton = (Button) findViewById(R.id.Bscan);
         boton.setOnClickListener(new OnClickListener() {
@@ -89,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 deviceName = device.getName();
                 deviceHardwareAddress = device.getAddress();
-
+                rssi_msg = (TextView) findViewById(R.id.textView);
                 endnow = System.nanoTime();
                 timeEnd = (endnow - startnow)/1000000;
                // rssi_msg.setText( "\n" + deviceHardwareAddress +  " | " + rssi_msg.getText()  + " | " +  deviceName + " |  "  + rssi + " dBm"   +  " | time process: " + timeEnd+ " milliseconds" );
@@ -100,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
 
-                        System.out.println(response.toString());
+                        //System.out.println(response.toString());
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -120,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
                         return parameters;
                     }
+
                 };
                 requestQueue.add(request);
             }
