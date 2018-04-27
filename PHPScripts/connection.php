31fa2af48b867f5eedf2a85475ce95f0bafcda21
yakatak5 <yuskhan@gmail.com>
2018-04-27 11:34:01 -0500

    all php scripts here

---
+++ PHPScripts/connection.php
@@ -0,0 +1,7 @@
+<?php
+define('hostname', '10.176.138.38:3307');
+define('user', 'root');
+define('password', 'appscale123');
+define('databaseName', 'bledata');
+$connect = mysqli_connect(hostname, user, password, databaseName);
+?>
